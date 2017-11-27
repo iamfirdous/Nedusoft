81641b06521a639652de1b90e32fcf0c11d7550e
Firdous I <mohd.firdous1995@gmail.com>
2017-11-27 18:30:59 +0530

    From NexusInfo Server

--- app/src/main/java/com/nexusinfo/nedusoft/ui/activities/SchoolCodeRequestActivity.java
+++ app/src/main/java/com/nexusinfo/nedusoft/ui/activities/SchoolCodeRequestActivity.java
@@ -1,27 +1,95 @@
 package com.nexusinfo.nedusoft.ui.activities;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.nexusinfo.nedusoft.MainActivity;
+import com.nexusinfo.nedusoft.ConnectionClass;
 import com.nexusinfo.nedusoft.R;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SchoolCodeRequestActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    Button button;
+    EditText editText;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_code_request);
 
-        Button button = findViewById(R.id.button_school_code_submit);
+        progressBar = findViewById(R.id.progressBar_school_code);
+        progressBar.setVisibility(View.GONE);
+        editText = findViewById(R.id.editText_school_code);
+        button = findViewById(R.id.button_school_code_submit);
+
         button.setOnClickListener(view -> {
-            Intent intent = new Intent(SchoolCodeRequestActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
+//            Intent intent = new Intent(SchoolCodeRequestActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//            finish();
+
+            SampleTask task = new SampleTask();
+            task.execute();
         });
+    }
+
+    class SampleTask extends AsyncTask<String, String, String>{
+
+        String out;
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            button.setEnabled(false);
+            editText.setEnabled(false);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                List<String> list = new ArrayList<>();
+                Connection conn = ConnectionClass.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM " + ConnectionClass.NEDUSOFT_TABLE);
+                if(rs != null){
+                    while (rs.next()){
+                        list.add(rs.getString(ConnectionClass.DATABASE_NAME));
+                    }
+                    publishProgress(list.get(0), list.get(1));
+                }
+                else {
+                    Log.e("Error", "rs is null");
+                }
+            }
+            catch (Exception e){
+                Log.e("Exception", e.toString());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            Toast.makeText(SchoolCodeRequestActivity.this, "Database for 0001: " + values[0] + "\nDatabase for 0002: " + values[1], Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressBar.setVisibility(View.GONE);
+            button.setEnabled(true);
+            editText.setEnabled(true);
+        }
     }
 }
